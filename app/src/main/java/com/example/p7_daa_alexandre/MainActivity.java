74c928aec5ca27551b3474729fa301ec65365316
Clémençot Alexandre <73541197+Alexandre-C-89@users.noreply.github.com>
2023-11-20 11:19:26 +0100

    Add styles and string

--- app/src/main/java/com/example/p7_daa_alexandre/MainActivity.java
+++ app/src/main/java/com/example/p7_daa_alexandre/MainActivity.java
@@ -15,41 +15,29 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class MainActivity<A extends ViewBinding> extends AppCompatActivity {
+public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
     private ActivityMainBinding binding;
 
     private static final int RC_SIGN_IN = 123;
-    private GoogleSignInClient googleSignInClient;
-
-    private FirebaseAuth firebaseAuth;
-
-    private static final String TAG = "GOOGLE_SIGN_IN_TAG";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    ActivityMainBinding getViewBinding() {
+        return null;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-
-        HomeFragment homeFragment = new HomeFragment();
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container_fragment, homeFragment)
-                .addToBackStack(null)
-                .commit();
-
-        //mainViewModel = new ViewModelProvider(this, ViewModelFactory.getInstance(this)).get(MainViewModel.class);
-
-        // Appel de la fonction pour la connexion
         setupListeners();
-
-        setContentView(binding.getRoot());
     }
 
     private void setupListeners(){
         // Login Button
-        binding.loginBtn.setOnClickListener(view -> {
+        binding.loginButton.setOnClickListener(view -> {
             startSignInActivity();
         });
     }
@@ -58,61 +46,18 @@ public class MainActivity<A extends ViewBinding> extends AppCompatActivity {
 
         // Choose authentication providers
         List<AuthUI.IdpConfig> providers =
-                Arrays.asList(
-                new AuthUI.IdpConfig.GoogleBuilder().build(),
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.TwitterBuilder().build());
+                Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
 
         // Launch the activity
-        startActivity(
+        startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
-                        .setTheme(R.style.loginTheme)
+                        .setTheme(R.style.LoginTheme)
                         .setAvailableProviders(providers)
                         .setIsSmartLockEnabled(false, true)
-                        .setLogo(R.drawable.logo_white)
+                        .setLogo(R.drawable.ic_logo_auth)
                         .build(),
                 RC_SIGN_IN);
     }
 
-    // Show Snack Bar with a message
-    private void showSnackBar( String message){
-        Snackbar.make(binding.mainLayout, message, Snackbar.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        this.handleResponseAfterSignIn(requestCode, resultCode, data);
-    }
-
-    // Method that handles response after SignIn Activity close
-    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
-
-        IdpResponse response = IdpResponse.fromResultIntent(data);
-
-        if (requestCode == RC_SIGN_IN) {
-            // SUCCESS
-            if (resultCode == RESULT_OK) {
-                userManager.createUser();
-                showSnackBar(getString(R.string.connection_succeed));
-            } else {
-                // ERRORS
-                if (response == null) {
-                    showSnackBar(getString(R.string.error_authentication_canceled));
-                } else if (response.getError()!= null) {
-                    if(response.getError().getErrorCode() == ErrorCodes.NO_NETWORK){
-                        showSnackBar(getString(R.string.error_no_internet));
-                    } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
-                        showSnackBar(getString(R.string.error_unknown_error));
-                    }
-                }
-            }
-        }
-    }
-    // Show Snack Bar with a message
-    private void showSnackBar( String message){
-        //Snackbar.make(binding.mainLayout, message, Snackbar.LENGTH_SHORT).show();
-        Toast.makeText( this, message, Toast.LENGTH_SHORT).show();
-    }
 }
