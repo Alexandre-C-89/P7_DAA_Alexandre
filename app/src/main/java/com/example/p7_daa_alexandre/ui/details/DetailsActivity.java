f8feb64ce92f2421fc0f363174bf1191885165ca
Clémençot Alexandre <73541197+Alexandre-C-89@users.noreply.github.com>
2024-03-04 18:53:51 +0100

    clean code

--- app/src/main/java/com/example/p7_daa_alexandre/ui/details/DetailsActivity.java
+++ app/src/main/java/com/example/p7_daa_alexandre/ui/details/DetailsActivity.java
@@ -33,8 +33,6 @@ public class DetailsActivity extends AppCompatActivity {
 
     private DetailsViewModel viewModel;
 
-    private RecyclerView recyclerView;
-
     private DetailsAdapter adapter;
 
     private DetailsResponse details;
@@ -60,7 +58,6 @@ public class DetailsActivity extends AppCompatActivity {
                 binding.nameRestaurant.setText(details.getResult().getName().toLowerCase(Locale.ROOT));
                 binding.adresseResto.setText(details.getResult().getVicinity());
                 binding.rating.setRating(details.getResult().getRating());
-                //binding.adresseResto.setText(details.getResult().getPhotos());
 
                 String urlPhoto = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=" + details.getResult().getPhotos().get(0).getPhotoReference() + "&key=AIzaSyCdjoEFb1ArPZYQBXpBdkkmIMdUaGycFow";
 
@@ -85,49 +82,32 @@ public class DetailsActivity extends AppCompatActivity {
         binding.phoneButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                // Vérifie si le numéro de téléphone est disponible
                 if (details.getResult().getFormattedPhoneNumber() != null && !details.getResult().getFormattedPhoneNumber().isEmpty()) {
-                    // Crée l'intent pour composer le numéro
                     Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + details.getResult().getFormattedPhoneNumber()));
-
-                    // Vérifie si l'application pour gérer l'intent est disponible
                     if (dialIntent.resolveActivity(getPackageManager()) != null) {
-                        // Lance l'intent pour composer le numéro
                         startActivity(dialIntent);
                     } else {
-                        // Gère le cas où aucune application pour gérer l'intent n'est disponible
                         Toast.makeText(DetailsActivity.this, "Aucune application pour gérer l'appel téléphonique n'est disponible.", Toast.LENGTH_SHORT).show();
                     }
                 } else {
-                    // Gère le cas où le numéro de téléphone n'est pas disponible
                     Toast.makeText(DetailsActivity.this, "Aucun numéro de téléphone disponible.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
         // clique sur website
-
         binding.websiteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Assurez-vous que l'URL du site web est disponible dans les détails du restaurant
                 if (details != null && details.getResult().getWebsite() != null && !details.getResult().getWebsite().isEmpty()) {
-                    // Récupérez l'URL du site web du restaurant
                     String websiteUrl = details.getResult().getWebsite();
-
-                    // Créez l'intent pour ouvrir le site web
                     Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(websiteUrl));
-
-                    // Vérifiez si l'application pour gérer l'intent est disponible
                     if (webIntent.resolveActivity(getPackageManager()) != null) {
-                        // Lancez l'intent pour ouvrir le site web
                         startActivity(webIntent);
                     } else {
-                        // Gérez le cas où aucune application pour gérer l'intent n'est disponible
                         Toast.makeText(DetailsActivity.this, "Aucune application pour ouvrir le site web n'est disponible.", Toast.LENGTH_SHORT).show();
                     }
                 } else {
-                    // Gérez le cas où l'URL du site web n'est pas disponible
                     Toast.makeText(DetailsActivity.this, "Aucun site web disponible pour ce restaurant.", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -136,10 +116,10 @@ public class DetailsActivity extends AppCompatActivity {
         // click on button choice
         binding.choiceButton.setOnClickListener(v -> {
             if (details != null) {
-                String placeId = details.getResult().getPlaceId(); // take ID of restaurant
-                String restaurantName = details.getResult().getName(); // take name of restaurant
-                String address= details.getResult().getFormattedAddress(); // take address of restaurant
-                viewModel.restaurantChoosed(placeId, restaurantName, address); // call method of ViewModel for adding favorite restaurant
+                String placeId = details.getResult().getPlaceId();
+                String restaurantName = details.getResult().getName();
+                String address= details.getResult().getFormattedAddress();
+                viewModel.restaurantChoosed(placeId, restaurantName, address);
                 Toast.makeText(DetailsActivity.this, "Restaurant ajouté à vos favoris", Toast.LENGTH_SHORT).show();
             }
         });
