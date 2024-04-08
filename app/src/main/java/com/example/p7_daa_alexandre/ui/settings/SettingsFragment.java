4b9238caedb6d3264bce9db652496f5b3f684414
AlexQobuz <alexandre.clemencot@qobuz.com>
2024-04-08 17:05:33 +0200

    correction error on notification.

--- app/src/main/java/com/example/p7_daa_alexandre/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/p7_daa_alexandre/ui/settings/SettingsFragment.java
@@ -40,18 +40,20 @@ public class SettingsFragment extends Fragment {
 
         viewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
 
-        binding.notificationBtn.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            viewModel.updateNotificationStatus(isChecked);
-            if (isChecked == true) {
-                Toast.makeText(getActivity(), "Notification activated !", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(getActivity(), "Notification disabled !", Toast.LENGTH_SHORT).show();
-            }
-        });
-
         viewModel.getNotificationStatus().observe(getViewLifecycleOwner(), isChecked -> {
             binding.notificationBtn.setChecked(isChecked);
+
+            binding.notificationBtn.setOnCheckedChangeListener((buttonView, Checked) -> {
+                viewModel.updateNotificationStatus(Checked);
+                if (Checked == true) {
+                    Toast.makeText(getActivity(), "Notification activated !", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), "Notification disabled !", Toast.LENGTH_SHORT).show();
+                }
+            });
+
         });
+
 
     }
 
