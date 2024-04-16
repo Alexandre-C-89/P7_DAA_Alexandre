1cd1c5f602a850dec287deaa36f78ab26cee2e4f
AlexQobuz <alexandre.clemencot@qobuz.com>
2024-04-16 15:25:32 +0200

    change gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.2.0")
     implementation("com.google.android.libraries.places:places:3.3.0")
     implementation("com.google.firebase:firebase-messaging:23.4.1")
+    implementation("com.google.firebase:firebase-firestore:24.11.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0-alpha03")
@@ -61,27 +62,29 @@ dependencies {
 
     // FIREBASE
     // Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     // FIREBASE UI
     implementation("com.firebaseui:firebase-ui-auth:7.2.0")
     // Add the dependency for the Firebase SDK for Google Analytics
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-messaging")
+    implementation("com.google.firebase:firebase-analytics")
+
 
     // Also add the dependency for the Google Play services library and specify its version
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
 
     //Glide
     implementation ("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.13.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.13.2")
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.code.gson:gson:2.10.1")
-    implementation ("com.squareup.okhttp3:okhttp:3.14.9")
+    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
     implementation ("com.squareup.retrofit2:converter-scalars:2.5.0")
 
     //ROOM
@@ -89,13 +92,13 @@ dependencies {
     annotationProcessor ("androidx.room:room-compiler:2.6.1")
 
     // HILT
-    implementation ("com.google.dagger:hilt-android:2.45")
-    annotationProcessor ("com.google.dagger:hilt-compiler:2.45")
+    implementation ("com.google.dagger:hilt-android:2.51")
+    annotationProcessor ("com.google.dagger:hilt-compiler:2.51")
 
     // GOOGLE MAPS SDK
     implementation ("com.google.android.gms:play-services-maps:18.2.0")
     implementation ("com.google.maps.android:android-maps-utils:2.3.0")
-    implementation ("com.google.android.libraries.places:places:3.3.0")
+    implementation ("com.google.android.libraries.places:places:3.4.0")
 
     // Notification
     implementation ("androidx.work:work-runtime-ktx:2.9.0")
